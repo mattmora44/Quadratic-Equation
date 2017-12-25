5ebb9129317168d933eef5126d6005be43062679
Matthew Mora <moramatt1@gmail.com>
2017-12-25 00:22:50 -0600

    added linear and real roots cases

--- main.cpp
+++ main.cpp
@@ -19,11 +19,26 @@ int main(){
 	}
 	double x1 = 0.0;
 	double x2 = 0.0;
-
+	cout << "roots of "<< a << "x^2 + ("<< b <<")x + ("<< c <<") :"<<endl;
 	//linear
+	if (a == 0){
+		x1 = findLroots(b,c);
+		cout << "x = "<< x1<<endl;
+	}
 	//2 real roots
+	if (b*b-4*a*c > 0){
+		x1 = findFirstroot(a,b,c);
+		x2 = findSecRoot(a,b,c);
+		cout <<"x1: "<<x1<<endl<<"x2: "<<x2<<endl;S
+	}
 	//idendical roots
+	if (b*b-4*a*c == 0){
+
+	}
 	//imaginary roots
+	if (b*b-4*a*c < 0){
+
+	}
 
 	catch (runtime_error& e){
 		cout<<endl<<e.what()<<endl;
