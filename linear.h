803e27c197e166651705bc5a17b3e98e8f101d7b
Matthew Mora <moramatt1@gmail.com>
2017-12-25 13:09:50 -0600

    fixed compilation errors

---
+++ linear.h
@@ -0,0 +1,6 @@
+#ifndef LINEAR_H
+#define LINEAR_H
+
+double findLroots(double b, double c);
+
+#endif
---
+++ tworealroots.h
@@ -0,0 +1,7 @@
+#ifndef TWOREALROOTS_H
+#define TWOREALROOTS_H
+
+double findFirstRoot(double a, double b, double c);
+double findSecRoot(double a, double b, double c);
+
+#endif
